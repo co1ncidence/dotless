c1de2bc6e1d5665c3e585fb9bc36b11fc04911f3
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-09-05 22:19:50 -0500

    Sat Sep  5 10:19:49 PM CDT 2020

---
+++ .config/polybar/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -rq -c ~/.config/polybar/config.ini main &
+polybar -rq -c ~/.config/polybar/config.ini workspaces &
+polybar -rq -c ~/.config/polybar/config.ini utils &
