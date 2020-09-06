aa5c7b6f41ceac18098cf5749050f3ed5dc328e7
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-09-06 12:23:16 -0500

    Sun Sep  6 12:23:16 PM CDT 2020

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Apercu Pro Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Apercu Pro Mono:pixelsize=16:antialias=true:autohint=true";
 
 
 /* Spare fonts */
