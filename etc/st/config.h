c95b43e2eb0edcec8adbd32d25ddcf5f50c8ec8e
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-09-08 16:13:08 -0500

    Tue Sep  8 04:13:08 PM CDT 2020

--- etc/st/config.h
+++ etc/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Apercu Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Akkurat-Mono:pixelsize=16:antialias=true:autohint=true";
 
 
 /* Spare fonts */
