a1311999f3783c1acb8d491049e4771c2c895520
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-09-17 15:58:24 -0500

    docs:update

--- etc/st/config.h
+++ etc/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Apercu Pro Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 
 
 /* Spare fonts */
