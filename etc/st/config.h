34e0fc4b97196a639f8487c8fe4aa24c9c3a477c
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-09-27 00:04:00 -0500

    change to cute bitmap font

--- etc/st/config.h
+++ etc/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Recursive Mono Linear Static:pixelsize=13:antialias=true:autohint=true";
+static char *font = "envypn:pixelsize=14:antialias=true:autohint=true";
 
 
 /* Spare fonts */
