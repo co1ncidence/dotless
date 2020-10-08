9aa9eba371e772e36f20b13178f5e5fdbd9a185a
co1ncidence <r3yan.chaudhry@zohomail.com>
2020-10-07 21:48:32 -0500

    change font to input mono condensed

--- etc/st/config.h
+++ etc/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Space Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Input Mono Condensed:pixelsize=14:antialias=true:autohint=true";
 
 
 /* Spare fonts */
